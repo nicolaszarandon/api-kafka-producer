ff7dfcff90819f07760cad56ea962e4b225006cf
Nicolas Zarandon <nicozarandon@gmail.com>
2022-11-01 12:51:39 -0300

    Base proyecto

---
+++ src/main/java/com/nz/api_kafka_producer/ApiKafkaProducerApplication.java
@@ -0,0 +1,13 @@
+package com.nz.api_kafka_producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiKafkaProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiKafkaProducerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nz/api_kafka_producer/ApiKafkaProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nz.api_kafka_producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiKafkaProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
