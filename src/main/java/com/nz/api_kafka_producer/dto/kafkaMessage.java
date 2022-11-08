0587bf89cc47573802f27d309a323721e1890e5b
Nicolas Zarandon <nicozarandon@gmail.com>
2022-11-08 11:31:01 -0300

    funcionando

--- src/main/java/com/nz/api_kafka_producer/dto/kafkaMessage.java
+++ src/main/java/com/nz/api_kafka_producer/dto/kafkaMessage.java
@@ -1,18 +1,13 @@
 package com.nz.api_kafka_producer.dto;
 
-import com.nz.api_kafka_producer.product.Product;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-public class kafkaMessage{
+public class kafkaMessage<T>{
     public String action;
-    public Product data;
+    public T data;
 }
